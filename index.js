d9746bd469ab34c257ac663660a1faa18e616d82
Olya Kukareko <olya.kukareko@gmail.com>
2021-07-20 01:43:42 +0300

    feat: add slick slider connection

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({ //подключение slick slider
+        infinite: true,
+        speed: 500,
+        dots: true,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear'
+    });
+  });
