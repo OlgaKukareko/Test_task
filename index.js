ac1c394b088b5cfea4545cbea312791ca21303c7
Olya Kukareko <olya.kukareko@gmail.com>
2021-07-24 15:57:11 +0300

    add styles for slider, change width for dots,add classes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.slider').slick({ //подключение slick slider
         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
         speed: 500,
         dots: true,
         arrows: false,
